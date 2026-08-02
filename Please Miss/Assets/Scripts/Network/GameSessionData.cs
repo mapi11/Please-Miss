0ca81b89912d898e4b6e8ff6d7092d5cdcbb9c58
mapi.. <asatramax2204@mail.ru>
2026-08-02 22:05:00 +0300

    Небольшие правки на локациях

--- Please Miss/Assets/Scripts/Network/GameSessionData.cs
+++ Please Miss/Assets/Scripts/Network/GameSessionData.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public static class GameSessionData
 {
-    public const string GameVersion = "Please Miss 0.5";
+    public const string GameVersion = "Please Miss 0.6";
 
     public static string JoinCode = "";
     public static string ConnectionType = "";
